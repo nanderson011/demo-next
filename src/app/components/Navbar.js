ee4d67a9ade2833aa83663062dc264f7a7b5666e
nanderson011 <nanderson011@student.butte.edu>
2025-11-10 13:11:52 -0800

    assignment: tag component

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,12 +1,33 @@
 import Button from "./Button";
+import Tag from "./Tag"
+
+const navigationButtons = [
+  {
+    title: 'About',
+    href: '/about',
+    isNew: true
+  },
+  {
+    title: 'Home',
+    href: '/',
+    isNew: false
+  },
+
+]
 
 export default function Navbar() {
   return (
     <nav className="flex justify-between bg-indigo-50 p-6 items-center sticky top-0 shadow-lg">
       <div className="logo bg-indigo-600 rounded-full p-6 hover:scale-105 transition-all cursor-pointer"></div>
       <div className="flex gap-x-4 items-center">
-        <Button title="Home" href="/" />
-        <Button title="About" href="/about" />
+        {navigationButtons.map(({href, title, isNew}) => (
+          <>
+            <Button href={href} classNameProp="flex test-xl">
+              {isNew && <Tag classNames= "mr-2 animate-pulse" />}
+              {title}
+              </Button>
+          </>
+          ))}
       </div>
     </nav>
   )
